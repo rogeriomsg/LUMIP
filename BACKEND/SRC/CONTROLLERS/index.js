d70765ad5162333bd87ffd247773a728a58ac9cc
Rogerio <rogerio.till@eb.mil.br>
2025-01-02 00:41:02 -0300

    commit

--- BACKEND/SRC/CONTROLLERS/index.js
+++ BACKEND/SRC/CONTROLLERS/index.js
@@ -2,11 +2,13 @@ const Device = require('./device');
 const User = require('./user');
 const DeviceCategory = require('./deviceCategory');
 const Gateway  = require('./gateway');
+const WebhookToMQTT  = require('./webhookToMqtt');
 
 
 module.exports = { 
     Device , 
     User ,
     DeviceCategory,
-    Gateway
+    Gateway,
+    WebhookToMQTT,
 };
